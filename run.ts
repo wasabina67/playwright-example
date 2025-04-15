cc0e16e86ebdcb94ffc4129139f30de671b9f552
wasabina67 <wasabina67@gmail.com>
2025-04-15 21:33:52 +0900

    Update run.ts

--- run.ts
+++ run.ts
@@ -1,4 +1,15 @@
 import { chromium } from '@playwright/test';
 
 (async () => {
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+
+  await page.goto('https://example.com/');
+
+  const urls = await page.$$eval('a', (anchors) =>
+    anchors.map((anchor) => anchor.href).filter((href) => href.startsWith('http'))
+  );
+  console.log(urls);
+
+  await browser.close();
 })();
